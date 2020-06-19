cef8c6fcea751010f096a7d99d49497cf33d14db
JSchapke <joaoschapke@gmail.com>
2020-06-19 11:15:34 -0300

    updates

--- models/gat/params.py
+++ models/gat/params.py
@@ -1,47 +1,24 @@
-
-# (coli, string)
-gat_huge = {
+gat_yeast = {
     'lr': 0.005,
-    'weight_decay': 3e-4,
-    'h_feats': [128, 1], 
+    'weight_decay': 2e-4,
+    'h_feats': [12, 1], 
     'heads': [8, 1],  
-    'dropout': 0.4,
+    'dropout': 0.3,
     'negative_slope': 0.2 }
 
-# (coli, biogrid) 
-gat_large = {
-    'lr': 0.002,
-    'weight_decay': 3e-4,
-    'h_feats': [64, 1], 
-    'heads': [8, 1],  
-    'dropout': 0.4,
-    'negative_slope': 0.2 }
-
-# coli / yeast
-gat_medium = {
+gat_fly = {
     'lr': 0.005,
-    'weight_decay': 1e-4,
+    'weight_decay': 5e-4,
     'h_feats': [16, 1], 
-    'heads': [8, 1],  
-    'dropout': 0.1,
+    'heads': [1, 1],  
+    'dropout': 0.6,
     'negative_slope': 0.2 }
 
-# (coli, dip) (yeast, dip) (yeast, biogrid) (yeast, string)
-# humans / yeast
-gat_small = {
+gat_0 = {
     'lr': 0.005,
     'weight_decay': 5e-4,
     'h_feats': [8, 1], 
     'heads': [8, 1],  
     'dropout': 0.4,
-    'negative_slope': 0.2 }
-
-
-gat_simpler = {
-    'lr': 0.01,
-    'weight_decay': 5e-4,
-    'h_feats': [16, 1], 
-    'heads': [1, 1],  
-    'dropout': 0.6,
     'negative_slope': 0.2 }
 
