82645f69ad3ae4a7c63692d1bfa6a9b6864c1177
JSchapke <joaoschapke@gmail.com>
2020-12-05 11:07:35 -0300

    fixes bug in utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -44,7 +44,7 @@ def set_seed(seed):
 
 
 def dim_reduction_cor(X, y, k=20):
-    cors = np.zeros((X.shape[1])) 
+    cors = np.zeros((X.shape[1]))
 
     # calculate the correlation with y for each feature
     for i in range(X.shape[1]):
@@ -57,19 +57,16 @@ def dim_reduction_cor(X, y, k=20):
 
     return features, cors
 
-def filter_labels(labels, genes):
-    mask = np.isin(labels[:, 0], genes)
-    return labels[mask]
 
 def data(
-    organism='yeast',
-    ppi='string', 
-    expression=False, 
-    orthologs=False, 
-    sublocalizations=False,
-    string_thr=500, 
-    seed=0, 
-    weights=False):
+        organism='yeast',
+        ppi='string',
+        expression=False,
+        orthologs=False,
+        sublocalizations=False,
+        string_thr=500,
+        seed=0,
+        weights=False):
 
     assert(organism in ['yeast', 'coli', 'human', 'melanogaster'])
     assert(organism not in ['coli'] or not sublocalizations)
@@ -81,7 +78,8 @@ def data(
     # Cache ----------
     update = True
     cache = f'.cache/{organism}/'
-    cachepath = cache + f'{expression}_{sublocalizations}_{orthologs}_{ppi}.pkl'
+    cachepath = cache + \
+        f'{expression}_{sublocalizations}_{orthologs}_{ppi}.pkl'
     os.makedirs(cache, exist_ok=True)
 
     if os.path.isfile(cachepath) and not update:
@@ -93,11 +91,13 @@ def data(
         edge_weights = None
 
         if ppi in ['biogrid', 'dip']:
-            ppi_path = os.path.join(DATA_ROOT, f'essential_genes/{organism}/PPI/{ppi.upper()}/{ppi}.csv')
+            ppi_path = os.path.join(
+                DATA_ROOT, f'essential_genes/{organism}/PPI/{ppi.upper()}/{ppi}.csv')
             edges = pd.read_csv(ppi_path)
 
         elif ppi == 'string':
-            ppi_path = os.path.join(DATA_ROOT, f'essential_genes/{organism}/PPI/STRING/string.csv')
+            ppi_path = os.path.join(
+                DATA_ROOT, f'essential_genes/{organism}/PPI/STRING/string.csv')
             edges = pd.read_csv(ppi_path)
 
             key = 'combined_score'
@@ -107,86 +107,71 @@ def data(
             edges = edges[['A', 'B']]
             print('Filtered String network with thresh:', string_thr)
 
-
         edges = edges.dropna()
         index, edges = edges.index, edges.values
         ppi_genes = np.union1d(edges[:, 0], edges[:, 1])
-        if edge_weights is not None: 
+        if edge_weights is not None:
             edge_weights = edge_weights.iloc[index.values].values
 
-        path = os.path.join(DATA_ROOT, f'essential_genes/{organism}/EssentialGenes/ogee.csv')
-        labels = pd.read_csv(path).values
+        path = os.path.join(
+            DATA_ROOT, f'essential_genes/{organism}/EssentialGenes/ogee.csv')
+        labels = pd.read_csv(path).set_index('Gene')
 
         # filter labels not in the PPI network
         print('Number of labels before filtering:', len(labels))
-        labels = filter_labels(labels, ppi_genes)
+        labels = labels.loc[np.intersect1d(labels.index, ppi_genes)].copy()
         print('Number of labels after filtering:', len(labels))
 
-        genes = np.union1d(labels[:, 0], ppi_genes)
+        genes = np.union1d(labels.index, ppi_genes)
         print('Total number of genes:', len(genes))
 
         X = np.zeros((len(genes), 0))
+        X = pd.DataFrame(X, index=genes)
 
         if orthologs:
-            path = os.path.join(DATA_ROOT, f'essential_genes/{organism}/Orthologs/orthologs.csv')
-            orths = pd.read_csv(path)
+            path = os.path.join(
+                DATA_ROOT, f'essential_genes/{organism}/Orthologs/orthologs.csv')
+            orths = pd.read_csv(path).set_index('Gene')
+            X = X.join(orths, how="left")
             print('Orthologs dataset shape:', orths.shape)
-
-            x = np.zeros((len(genes), orths.shape[1]-1))
-            for i, node in enumerate(genes):
-                mask = orths['Gene'] == node
-                if np.any(mask):
-                    x[i] = orths[mask].values.squeeze()[:-1]
-            X = np.concatenate([X, np.array(x)], axis=1)
 
         if expression:
             if organism == 'human':
-                path = os.path.join(DATA_ROOT, 'essential_genes/human/Expression/expression_64.csv')
+                path = os.path.join(
+                    DATA_ROOT, 'essential_genes/human/Expression/expression_64.csv')
             else:
-                path = os.path.join(DATA_ROOT, f'essential_genes/{organism}/Expression/profile.csv')
-            expression = pd.read_csv(path).values
+                path = os.path.join(
+                    DATA_ROOT, f'essential_genes/{organism}/Expression/profile.csv')
+            expression = pd.read_csv(path).set_index('Gene')
+            X = X.join(expression, how="left")
             print('Gene expression dataset shape:', expression.shape)
 
-            x = np.zeros((len(genes), expression.shape[1]-1))
-            for i, gene in enumerate(genes):
-                mask = expression[:, 0] == gene
-                if np.any(mask):
-                    x[i] = expression[mask][0, 1:]
-            X = np.concatenate([X, x], axis=1)
-
-
         if sublocalizations:
-            path = os.path.join(DATA_ROOT, f'essential_genes/{organism}/SubLocalizations/subloc.csv')
-            subloc = pd.read_csv(path).values
+            path = os.path.join(
+                DATA_ROOT, f'essential_genes/{organism}/SubLocalizations/subloc.csv')
+            subloc = pd.read_csv(path).set_index('Gene')
+            X = X.join(subloc, how="left")
             print('Subcellular Localizations dataset shape:', subloc.shape)
-
-            x = np.zeros((len(genes), subloc.shape[1]-1))
-            for i, node in enumerate(genes):
-                mask = subloc[:, 0] == node
-                #print(subloc[:,0], node)
-                if np.any(mask):
-                    print(subloc[mask, 1:].astype(float).max(0))
-                    x[i] = subloc[mask, 1:].astype(float).max(0)
-            X = np.concatenate([X, np.array(x)], axis=1)
 
         # Cache ----------
         with open(cachepath, 'wb') as f:
             pickle.dump([edges, edge_weights, X, labels, genes], f, protocol=2)
         # ----------------
 
-    train, test = train_test_split(labels, test_size=0.2, random_state=seed, stratify=labels[:, 1])
+    train, test = train_test_split(
+        labels, test_size=0.2, random_state=seed, stratify=labels)
 
     print(f'Num nodes {len(genes)} ; num edges {len(edges)}')
     print(f'X.shape: {None if X is None else X.shape}.')
-    print(f'Train labels. Num: {len(train)} ; Num pos: {train[:,1].sum()}')
-    print(f'Test labels. Num: {len(test)} ; Num pos: {test[:,1].sum()}')
+    print(f'Train labels. Num: {len(train)} ; Num pos: {train.Label.sum()}')
+    print(f'Test labels. Num: {len(test)} ; Num pos: {test.Label.sum()}')
     return (edges, edge_weights), X, train, test, genes
 
 
 if __name__ == '__main__':
     edge_info, X, train, test, genes = data(
-                organism='melanogaster', 
-                ppi='string', 
-                expression=True,
-                sublocalizations=False,
-                orthologs=True)
+        organism='melanogaster',
+        ppi='string',
+        expression=False,
+        sublocalizations=True,
+        orthologs=False)
